40b7199fe858f1f5f0bc1ee66a028ff29a69f401
Theodore Bellas <3573717-theodore.bellas@users.noreply.gitlab.com>
2016-12-11 07:49:40 -0500

    Removed old form that was on top to control all-inclusive toggle stuff. Reworked design for singlelight (CSS/HTML, centered them, fixed actions). Added loading animation to togglePowerSingle. Added transition animation to power button. Added classes for on and off (find a better way to do this)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,11 @@
 @extends('master') @section('content')
 
-
-<div class="row">
-    <div class="col-md-12">
-        <button type="button" id="listLifx">
-  List Lights
-</button>
-
-        <button type="button" id="changeLifxState">
-  Toggle On/Off
-</button>
-
-        <button type="button" id="breatheLifxLights">
-  Breathe All Lights
-</button>
+<div class="row master-row">
 
 
-        <div>
-
-            <label>Light Number</label>
-            <input type="number" class="light-prop required"  id="lightIndex"><br>
-
-            <label>Light brightness</label>
-            <input type="number" class="light-prop required" id="lightBrightness"><br>
-
-            <label>Light Temperature</label>
-            <input type="number" class="light-prop required" id="lightTemp"><br>
-
-            <button id="setState">
-  Set State
-</button>
-        </div>
-
-
-    </div>
 </div>
-<div class="row">
+
+<div class="row" id="light-control-row">
     @foreach($lights as $light)
 
       @include('partials.singlelight', $light)
--- resources/views/partials/singlelight.blade.php
+++ resources/views/partials/singlelight.blade.php
@@ -1,30 +1,51 @@
-<div class="light-container col-md-3 col-sm-6">
 
-    <p class="power-icon">
-      <a href="#" class="light-action-power" data-light-id="{{ $light['id'] }}" >
-        <i class="fa fa-power-off light-power-{{ $light['power'] }}" aria-hidden="true"></i>
-      </a>
-    </p>
-    <p class="light-label">
-        Label: <b>{{ $light['label'] }}</b>
-    </p>
-    <p class="light-product">
-        {{ $light['product']['company'] }} {{ $light['product']['name'] }}
-    </p>
-    <p class="light-status">
-      @if($light['connected'] == 1)
-        Status: <span class="light-status-connected">Connected!</span> <br>
-        @if($light['power'] == "on")
-          Power: <span class="light-status-connected">On</span>
-        @else
-          Power: <span class="light-status-offline">Off</span>
-        @endif
-      @else
-        Status: <span class="light-status-offline">Offline</span>
-      @endif
+  <div class="light-container col-md-2 col-centered col-sm-4" id="{{ $light['id'] }}">
+    <div class="light-inner">
+      <h3 class="light-label-header label-header-{{$light['power'] }}">
+        <b>{{ $light['label'] }}</b>
+      </h3>
 
-    </p>
-    <p class="light-status">
 
-    </p>
-</div>
+      <div class="light-content">
+
+        <div class="power-icon">
+          @if($light['connected'] == 1)
+            <a href="#" class="light-action light-action-power" data-action="togglePowerSingle" data-light-id="{{ $light['id'] }}" >
+              <i class="fa fa-power-off light-power-{{ $light['power'] }}" aria-hidden="true"></i>
+            </a>
+          @else
+            <a href="#" class="light-discon" data-action="togglePowerSingle" data-light-id="{{ $light['id'] }}" >
+              <i class="fa fa-plug light-power-{{ $light['power'] }}" aria-hidden="true"></i>
+            </a>
+          @endif
+        </div>
+
+        <p class="light-product">
+          {{ $light['product']['company'] }} {{ $light['product']['name'] }}
+        </p>
+        <p class="light-status">
+
+          @if($light['connected'] == 1)
+            Status: <span class="light-status-connected">Connected!</span>
+            @if($light['power'] == "on")
+              Power: <span class="light-status-connected">On</span>
+            @else
+              Power: <span class="light-status-offline">Off</span>
+            @endif
+          @else
+            Status: <span class="light-status-offline">Offline</span>
+          @endif
+
+        </p>
+
+
+
+      </div>
+
+      </div>
+      <div class="light-loader initial_hidden">
+        <i class="fa fa-gear fa-spin "></i>
+      </div>
+
+
+  </div>
